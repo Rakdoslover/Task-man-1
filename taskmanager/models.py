70c986f3b6cbbd31435b53b40855191a56b56359
Oscar Lundsör <oscarlundsor@gmail.com>
2023-06-01 09:01:56 +0000

    made some changes

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,7 +2,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    #schema for the category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
@@ -11,8 +11,9 @@ class Category(db.Model):
         # __repr__ to represent itself in the form of a string
         return self.category_name
 
+
 class Task(db.Model):
-    #schema for the task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -22,6 +23,6 @@ class Task(db.Model):
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return "#{0} - Task: {1} | Urgent {2}".format(
+        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
